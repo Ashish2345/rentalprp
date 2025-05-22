a5bce89e35280e52800584cdba49295e116ca91b
Ashish2345 <ashishrayamajhi227@gmail.com>
2025-05-22 23:32:59 +0545

    chesn

--- NepalRental/settings.py
+++ NepalRental/settings.py
@@ -40,7 +40,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'khatriami706@gmail.com'
+EMAIL_HOST_USER = 'khatriamit706@gmail.com'
 EMAIL_HOST_PASSWORD = 'aumd chfc duzk gjfz'
 
 
